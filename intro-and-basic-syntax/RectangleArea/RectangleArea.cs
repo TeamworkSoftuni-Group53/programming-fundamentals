690b0596e5ab5a4d7baaab351c584473a8a57ce8
Stefan Antonov <vertagend@gmail.com>
2018-03-11 15:50:07 +0200

    Documented solution of RectangleArea

--- intro-and-basic-syntax/RectangleArea/RectangleArea.cs
+++ intro-and-basic-syntax/RectangleArea/RectangleArea.cs
@@ -6,11 +6,12 @@ namespace RectangleArea
     {
         static void Main(string[] args)
         {
+			// We get an input of the height and width of a rectangle
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
-
+			// We calculate the area of the rectangle, using the input data
             double result = width * height;
-
+			// We print the result, formatted to up to 2 floating point numbers
             Console.WriteLine($"{result:f2}");
         }
     }
