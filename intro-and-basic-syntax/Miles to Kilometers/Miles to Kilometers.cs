c287d2720450079566fad7387d102fbae1d8ba78
nTrifonoff <trifonoff@mail.bg>
2018-03-11 14:39:42 +0200

    Documented the code Solution of Problem 3

--- intro-and-basic-syntax/Miles to Kilometers/Miles to Kilometers.cs
+++ intro-and-basic-syntax/Miles to Kilometers/Miles to Kilometers.cs
@@ -6,9 +6,13 @@ namespace Miles_to_Kilometers
     {
         static void Main(string[] args)
         {
+            //We are receiving a floating number from the Console for our kilometers
             double kilometers = double.Parse(Console.ReadLine());
 
+            //We are converting our kilometers into miles
             double miles = kilometers * 1.60934;
+
+            //We are printing the result
             Console.WriteLine($"{miles:f2}");
         }
     }
