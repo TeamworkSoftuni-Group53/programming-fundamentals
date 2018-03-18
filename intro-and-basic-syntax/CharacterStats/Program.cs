0fd6f4953af05ff8763d77fb184bde16ce6c8216
nTrifonoff <trifonoff@mail.bg>
2018-03-18 14:48:59 +0200

    Added solutions for Problem 4 and Problem 5 Renamed solution of Problem 3

---
+++ intro-and-basic-syntax/CharacterStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            string healthBar = "|" + new string('|', currentHealth) + new string('.', maxHealth - currentHealth) + "|";
+            string energyBar = "|" + new string('|', currentEnergy) + new string('.', maxEnergy - currentEnergy) + "|";
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: {healthBar}");
+            Console.WriteLine($"Energy: {energyBar}");
+        }
+    }
+}
