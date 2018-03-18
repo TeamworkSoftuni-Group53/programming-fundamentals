3af5274b975cbd25c5d05fefdc663661d85db37d
nTrifonoff <trifonoff@mail.bg>
2018-03-18 15:01:40 +0200

    Documented the solutions of Problem 4 and Problem 5

---
+++ intro-and-basic-syntax/BeverageLabels/BeverageLabels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We receive a name of product.
+            string name = Console.ReadLine();
+
+            //In the next three lines we receive amount, energy content and sugar content as an integers.
+            int amount = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            //In the next two lines we calculate the energy and sugar content.
+            double energySum = (double)amount * energyContent / 100;
+            double sugarSum = (double)amount * sugarContent / 100;
+
+            //We print the amount of the product and its name!
+            Console.WriteLine($"{amount}ml {name}:");
+
+            //We print the total energy and sugar content.
+            Console.WriteLine($"{energySum}kcal, {sugarSum}g sugars");
+        }
+    }
+}
---
+++ intro-and-basic-syntax/CharacterStats/CharacterStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We receive the name of our character.
+            string name = Console.ReadLine();
+
+            //In the next four lines we receive the current health, maximum health, current energy and maximum energy.
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            //In then next two lines we are creating the health and energy bar of our character.
+            string healthBar = "|" + new string('|', currentHealth) + new string('.', maxHealth - currentHealth) + "|";
+            string energyBar = "|" + new string('|', currentEnergy) + new string('.', maxEnergy - currentEnergy) + "|";
+
+            //in then text three lines we print the name, health and energy bar of our character.
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: {healthBar}");
+            Console.WriteLine($"Energy: {energyBar}");
+        }
+    }
+}
